3ae1ec5e162bdae00334f8fab24558ea9805bccd
brent <brent@DESKTOP-H3MO9B7>
2020-02-18 18:42:15 +0100

    Initial commit

---
+++ timeofday/src/timeofday/TimeOfDay.java
@@ -0,0 +1,28 @@
+package timeofday;
+
+public class TimeOfDay {
+	
+	/**
+	 * 
+	 */
+	
+	private int hours;
+	private int minutes;
+	int getHours() {return hours;}
+	int getMinutes() {return minutes;}
+	
+	/**
+	 * 
+	 * 
+	 */
+	
+	void setHours(int hours) {this.hours = hours;}
+	
+	/**
+	 * 
+	 *
+	 */
+	
+	void setMinutes(int minutes) {this.minutes = minutes;}
+
+}
